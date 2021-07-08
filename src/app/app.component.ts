877d92d4164ad1e50aff91c9dfd6e51165f5fe66
Firestorm270 <david.fettinger@gmail.de>
2021-07-08 10:12:06 +0200

    Aufteilung in 3 Seiten Unterteilung in 4 Einzelteile Hinzufügen versch. Komponenten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatChipInputEvent} from "@angular/material/chips";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngebotsSeiten';
+  panelOpenState: boolean | undefined;
+  dataSource: any;
+  displayedColumns: any;
+  availableColors: any;
+  textbausteine: any;
+  selectable: any;
+  removable: any;
+  TextbausteinCtrl: any;
+  separatorKeysCodes: any;
+  task: any;
+  allComplete: any;
+  toppings: any;
+  toppingList: any;
+
+  applyFilter($event: KeyboardEvent) {
+
+  }
+
+  remove(fruit: any) {
+
+  }
+
+  add($event: MatChipInputEvent) {
+
+  }
+
+  selected($event: MatAutocompleteSelectedEvent) {
+
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule } from './app-routing.module';
+import {AppComponent } from './app.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {MatChipsModule} from "@angular/material/chips";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatExpansionModule,
+    NoopAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
